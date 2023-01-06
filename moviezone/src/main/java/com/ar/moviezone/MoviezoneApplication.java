0d1a70c1d3c190ef203b25bb9c633569a243f772
Arkya Patwa <arkyapatwa333@gmail.com>
2023-01-06 22:47:57 +0530

    adding the boiler plate code

---
+++ moviezone/src/main/java/com/ar/moviezone/MoviezoneApplication.java
@@ -0,0 +1,13 @@
+package com.ar.moviezone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviezoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviezoneApplication.class, args);
+	}
+
+}
---
+++ moviezone/src/test/java/com/ar/moviezone/MoviezoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ar.moviezone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviezoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

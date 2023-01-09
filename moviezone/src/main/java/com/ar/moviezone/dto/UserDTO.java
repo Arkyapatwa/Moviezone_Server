6da2bad4df9d61c50bfe695eda2bfc25680cf68e
Arkya Patwa <arkyapatwa333@gmail.com>
2023-01-09 16:07:32 +0530

    Fully Implemented UserDTO class

--- moviezone/src/main/java/com/ar/moviezone/dto/UserDTO.java
+++ moviezone/src/main/java/com/ar/moviezone/dto/UserDTO.java
@@ -1,5 +1,50 @@
 package com.ar.moviezone.dto;
 
 public class UserDTO {
+	
 	private Integer userId;
+	private String name;
+	private String emailId;
+	private String password;
+	private String phoneNumber;
+	private String address;
+	
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
 }

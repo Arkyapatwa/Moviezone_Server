fc774dc01772bc551b0ffe9e77678ee9c9486552
Arkya Patwa <arkyapatwa333@gmail.com>
2023-01-10 20:37:29 +0530

    Added entity package , partially implemented User and Movie entity class

---
+++ moviezone/src/main/java/com/ar/moviezone/entity/Movie.java
@@ -0,0 +1,58 @@
+package com.ar.moviezone.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Movie {
+	@Id
+	private Integer movieId;
+
+	private String name;
+	
+	private String movieLength;
+	
+	private String language;
+	
+	private String type;
+	
+	public Integer getMovieId() {
+		return movieId;
+	}
+
+	public void setMovieId(Integer movieId) {
+		this.movieId = movieId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMovieLength() {
+		return movieLength;
+	}
+
+	public void setMovieLength(String movieLength) {
+		this.movieLength = movieLength;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+}
---
+++ moviezone/src/main/java/com/ar/moviezone/entity/User.java
@@ -0,0 +1,69 @@
+package com.ar.moviezone.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	private Integer userId;
+	
+	private String name;
+	
+	private String emailId;
+	
+	private String password;
+	
+	private String phoneNumber;
+	
+	private String address;
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}

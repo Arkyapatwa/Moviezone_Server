203ae267eb9c4ba2d7abfe31dd37c5b183321455
Arkya Patwa <arkyapatwa333@gmail.com>
2023-08-20 21:08:25 +0530

    developed the api for fetching movies by id or for all

--- moviezone/src/main/java/com/ar/moviezone/controller/MovieController.java
+++ moviezone/src/main/java/com/ar/moviezone/controller/MovieController.java
@@ -1,10 +1,19 @@
 package com.ar.moviezone.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ar.moviezone.dto.MovieDTO;
+import com.ar.moviezone.exception.MovieZoneException;
 import com.ar.moviezone.service.UserMovieService;
 
 @RestController
@@ -16,4 +25,16 @@ public class MovieController {
 		
 		@Autowired
 		private Environment environment;
+		
+		@GetMapping("/movies")
+		public ResponseEntity<List<MovieDTO>> getAllMovies() throws MovieZoneException {
+			List<MovieDTO> movieDTOList = userMovieService.getAllMovies();
+			return new ResponseEntity<>(movieDTOList, HttpStatus.OK);
+		}
+		
+		@GetMapping("/movie/{movieId}")
+		public ResponseEntity<MovieDTO> getMovieById(@PathVariable Integer movieId) throws MovieZoneException {
+			MovieDTO movieDTO = userMovieService.getMovieById(movieId);
+			return new ResponseEntity<>(movieDTO, HttpStatus.OK);
+		}
 }
